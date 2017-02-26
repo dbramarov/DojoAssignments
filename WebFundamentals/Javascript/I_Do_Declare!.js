e361461f4d912c08568cc35449f2076edd65bdb6
Dorian Bramarov <dbramarov@yahoo.com>
2017-02-26 10:07:15 -0500

    Added first JS file

---
+++ WebFundamentals/Javascript/I_Do_Declare!.js
@@ -0,0 +1,56 @@
+
+var num1 = 1;
+undefined
+var num2 = 2;
+undefined
+var num3 = 3;
+undefined
+var num4 = 4;
+undefined
+﻿
+console.log("First Number", num1);
+VM368:1 First Number 1
+undefined
+console.log("Second Number", num2);
+VM405:1 Second Number 2
+undefined
+console.log("Third Number", num3);
+VM432:1 Third Number 3
+undefined
+console.log("Fourth Number", num4);
+VM459:1 Fourth Number 4
+undefined
+​
+var firstName = "Dorian";
+undefined
+var lastName = "Bramarov";
+undefined
+var petName = "Rusty";
+undefined
+var instrument = "guitar";
+undefined
+
+console.log("My first name is: " + firstName);
+VM653:1 My first name is: Dorian
+undefined
+console.log("My last name is: " + lastName);
+VM694:1 My last name is: Bramarov
+undefined
+console.log("My pets name is: " + petName);
+VM758:1 My pets name is: Rusty
+undefined
+console.log("I play the " + instrument);
+VM853:1 I play the guitar
+undefined
+
+var yes = true;
+undefined
+var no = false;
+undefined
+
+console.log("Did I eat today? .." + no);
+VM943:1 Did I eat today? ..false
+undefined
+console.log(yes + "datttt");
+VM984:1 true datttt
+undefined
