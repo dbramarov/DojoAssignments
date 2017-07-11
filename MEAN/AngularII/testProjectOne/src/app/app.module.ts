b2a13220598d5f32a09bfe017130df4ca075d441
Dorian Bramarov <dbramarov@yahoo.com>
2017-07-11 17:55:52 -0400

    Updated Files in CD Assignments

--- MEAN/AngularII/testProjectOne/src/app/app.module.ts
+++ MEAN/AngularII/testProjectOne/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterializeModule } from 'ng2-materialize';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
